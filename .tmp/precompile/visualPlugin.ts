af796b53a25c43185866545fed43f0d5cb1425de
Daniele on DPiMac <daniele@flessiva.com>
2017-04-20 18:21:11 +0200

    v1.1.0

---
+++ .tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var SmartFilterBySQLBI1458262140625 = {
+        name: 'SmartFilterBySQLBI1458262140625',
+        displayName: 'Smart Filter by OKViz',
+        class: 'Visual',
+        version: '1.1.0',
+        apiVersion: '1.5.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.SmartFilterBySQLBI1458262140625.Visual(options),
+        custom: true
+    };
+}
